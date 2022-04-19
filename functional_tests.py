01735928fb08e9434f20fde0d97c152a594c8694
FanxyD <2930705040@qq.com>
2022-04-19 13:43:41 +0800

    First unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,52 @@
-from selenium import webdriver
+from selenium import webdriver          # (1)
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app, She goes to check out its homepage
+        self.browser.get('http://localhost:8000')    # (3)
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)         # (4)
+        self.fail('Finish the test!')
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+        
 
-assert 'Django' in browser.title
+# browser = webdriver.Chrome()            # (2)
+
+# # Edith has heard about a cool new online to-do app, She goes to check out its homepage
+# browser.get('http://localhost:8000')    # (3)
+
+# # She notices the page title and header mention to-do lists
+# assert 'To-Do' in browser.title         # (4)
+
+# She is invited to enter a to-do items staight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+
+# when she hints enter, the page updates, and now the page lists
+# "-1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item, She enters "Use
+# peacock feathers to make a fly" (Edith is very methodical)
+
+#The page updates again, and now shows both items on her list
+
+#Edith wonders whether the site will remember her list. The she sees
+# that the site has generated a unique URL for her -- ther is some
+# explanatory text to that effect
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+ #browser.quit()
+
