d3e6e5da76a0f86892a879a1c23e5fc517d44b9e
FanxyD <2930705040@qq.com>
2022-05-10 14:30:58 +0800

    after modification(refactored)

--- functional_tests.py
+++ functional_tests.py
@@ -32,51 +32,24 @@ class NewVisitorTest(unittest.TestCase):
         
         # when she hints enter, the page updates, and now the page lists
         # "-1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),"New to-do item did not appear in table"
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
-        
-        
         # There is still a text box inviting her to add another item, She enters "Use
         # peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-        
-
-# browser = webdriver.Chrome()            # (2)
-
-# # Edith has heard about a cool new online to-do app, She goes to check out its homepage
-# browser.get('http://localhost:8000')    # (3)
-
-# # She notices the page title and header mention to-do lists
-# assert 'To-Do' in browser.title         # (4)
-
-# She is invited to enter a to-do items staight away
-
-# She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
-
-# when she hints enter, the page updates, and now the page lists
-# "-1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item, She enters "Use
-# peacock feathers to make a fly" (Edith is very methodical)
-
-#The page updates again, and now shows both items on her list
-
-#Edith wonders whether the site will remember her list. The she sees
-# that the site has generated a unique URL for her -- ther is some
-# explanatory text to that effect
-
-# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
-
- #browser.quit()
 
